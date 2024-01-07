eb472a6a718f1678b8f24a07b0081b8972f1c537
Lucas <lucasbombaron@gmail.com>
2024-01-07 11:28:45 +0100

    Movix commit 1

---
+++ src/Homepage.js
@@ -0,0 +1,11 @@
+import './homepage.css';
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <h1 className='homepage__title'>Movix</h1>
+    </div>
+  );
+}
+
+export default Homepage;
