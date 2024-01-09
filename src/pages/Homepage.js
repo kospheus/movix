4a640ccd3f64cd435fc56f0d6eea9f71e7152f93
Lucas <lucasbombaron@gmail.com>
2024-01-09 17:00:09 +0100

    screen size hook

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,24 @@
+import React from 'react';
+
+//Hook
+import useWindowSize from '../hooks/useWindowSize';
+
 //Style
 import './homepage.scss';
 
 //Components
 import Hero from '../components/hero/hero';
 import HeaderMobile from '../components/headers/headerMobile';
+import HeaderDesktop from '../components/headers/headerDesktop';
 
 function Homepage() {
+
+  const size = useWindowSize();
+
   return (
     <div className="homepage">
-      <HeaderMobile />
+      {size.width < 900 && <HeaderMobile />}
+      {size.width > 900 && <HeaderDesktop />}
       <Hero />
     </div>
   );
