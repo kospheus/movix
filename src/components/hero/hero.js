67799f45e20de3c24652d774278591ab5549e001
Lucas <lucasbombaron@gmail.com>
2024-01-10 15:25:21 +0100

    hero > carousel

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,22 +1,11 @@
 //Components
-import { Searchbar } from '../atoms/searchbar/searchbar';
-
-//Assets
-import heroBackground from '../../assets/movixHero.png';
+import { HeroCarousel } from '../atoms/heroCarousel/heroCarousel';
 
 function Hero() {
 
     return (
         <div className='hero'>
-            <div className='hero__overlay'></div> {/* La div pour le dégradé */}
-            <img src={heroBackground}  className='hero__img' alt='Movix catalogue' />
-            <div className='hero__content'>
-                <h2 className='hero__content__title'>Trouvez le film que vous cherchez</h2>
-                <div className='hero__content__searchbar'>
-                    <Searchbar placeholder='Film, genre, acteur, plateforme...'/>
-                </div>
-                <p className='hero__content__text'>Fini les heures passées devant le catalogue de Netflix, Amazon Prime ou Canal+. Créez vos playlist, votre Wishlist, et partagez les avec vos amis !</p>
-            </div>
+            <HeroCarousel />
         </div>
     )
 }
